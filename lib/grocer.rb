d85608a733abff1e9a6c889c6940e8a2ba6c988f
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-15 18:03:29 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -25,8 +25,7 @@ def consolidate_cart(cart)
         [:item] => cart[i][:item],
         [:price] => cart[i][:price],
         [:clearance] => cart[i][:clearance],
-        [:count] => 1
-      }
+        [:count] => 1}
       
       new_array << new_cart_item
     end
