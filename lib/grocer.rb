126cf998a53926f896dd9b0c744976b5d3f62b00
eager-monitor-013e <phil.haines92@gmail.com>
2021-01-15 18:09:16 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,40 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require "pry"
 
+def find_item_by_name_in_collection(name, collection)
+  i = 0
+  while i < collection.length do
+    
+    if collection[i][:item] == name
+      return collection[i]
+    end
+    
+    i += 1
+  end
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  new_array = []
+  i = 0
+  while i < cart.length 
+    new_cart_item = find_item_by_name_in_collection(cart[i][:item], new_array)
+    
+    if new_cart_item
+      new_cart_item[:count] += 1
+    else
+      new_cart_item = {
+        :item => cart[i][:item],
+        :price => cart[i][:price],
+        :clearance => cart[i][:clearance],
+        :count => 1
+      }
+      
+      new_array << new_cart_item
+    end
+    
+    i += 1
+  end
+  
+  return new_array
 end
 
 
